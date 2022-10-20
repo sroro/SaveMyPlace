edae13f9b887410fc41204a3ac5cc10f9425345c
rodolphe <rodolphe.schnetzer@gmail.com>
2022-10-20 12:51:24 +0200

    Initial Commit

---
+++ SaveMyPlace/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaveMyPlace
+//
+//  Created by Rodolphe Schnetzer on 20/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
